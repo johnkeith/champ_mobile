c6ec3ec195b2afb170069cab1dc2f9ed1d4759d5
John Keith <johnkeith@outlook.com>
2015-11-09 19:43:36 -0500

    setup sass

--- www/js/app.js
+++ www/js/app.js
@@ -8,4 +8,11 @@ angular.module('champ', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+// what I've learned about the fb api
+// I will use implicit grant flow
+// store client_id in distributed app code is okay!
+// need to make requests with correct authorization headers - a bash 64 hash of client id and client secret
+// need to set expiry time of token on client side - hopefully can set lifetime
+
